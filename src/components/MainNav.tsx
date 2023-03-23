08a9c9009bb130577a1d9d76d55608100a142fbc
Zack Siri <me@zacksiri.dev>
2023-03-23 12:47:25 +0700

    Update class to className
    Signed-off-by: Zack Siri <me@zacksiri.dev>

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -21,8 +21,8 @@ const MainNav: FunctionComponent<MainNavProps> = (props) => {
     <Popover>
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <a href="/" class="-m-1.5 p-1.5">
-          <span class="sr-only">Instellar.app</span>
-          <img class="h-8 w-auto" src="/instellar-logo.png" alt="" />
+          <span className="sr-only">Instellar.app</span>
+          <img className="h-8 w-auto" src="/instellar-logo.png" alt="" />
         </a>
         <div className="-mr-2 flex items-center lg:hidden">
           <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-slate-700 hover:text-slate-500 focus:outline-none">
