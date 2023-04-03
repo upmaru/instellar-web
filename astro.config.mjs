0567d785ed9e9a475b799ec992b6a3d9ba5b39ea
Zack Siri <me@zacksiri.dev>
2023-04-03 23:07:07 +0700

    setup cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind(), react(), mdx()],
   markdown: {
     syntaxHighlight: 'shiki'
-  }
+  },
+  adapter: cloudflare()
 });
