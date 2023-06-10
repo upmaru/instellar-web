20fa4e437dae21519da8630750814a4b7430a6bf
Zack Siri <me@zacksiri.dev>
2023-06-10 18:16:08 +0700

    fix terraform widget

--- src/components/TerraformWidget.tsx
+++ src/components/TerraformWidget.tsx
@@ -37,12 +37,13 @@ function TerraformWidget(props: TerraformWidgetProps) {
       <div className="grid grid-cols-1 gap-6">
         {templates.map((template) => {
           return (
-            <div key={`template_${template.id}`} className="flex items-center space-x-3 rounded-lg border border-slate-700 bg-slate-800 px-6 py-5 shadow-sm hover:border-slate-600">
+            <div key={`template_${template.id}`} className="relative flex items-center space-x-3 rounded-lg border border-slate-700 bg-slate-800 px-6 py-5 shadow-sm hover:border-slate-600">
               <div className="flex-shrink-0">
                 <img className="h-10 w-10 rounded-full" src={template.icon} alt="" />
               </div>
               <div className="min-w-0 flex-1">
                 <a href='#' onClick={changeSelected} data-id={template.id} className="focus:outline-none">
+                  <span className="absolute inset-0" aria-hidden="true"></span>
                   <p className="text-sm font-medium text-white">{template.name}</p>
                   <p className="truncate text-sm text-slate-500">{template.description}</p>
                 </a>
