7e0a8d35d243064b9a1fbb6f38bf18eab52ad1ae
Zack Siri <me@zacksiri.dev>
2023-06-12 16:47:40 +0700

    try million

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import million from 'million/compiler';
 import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
@@ -16,6 +17,9 @@ export default defineConfig({
     syntaxHighlight: 'shiki',
     remarkPlugins: [remarkToc],
     rehypePlugins: [rehypeSlug]
+  },
+  vite: {
+    plugins: [million.vite({ mode: 'react', server: true })]
   },
   // adapter: node({
   //   mode: "standalone"
